0d8327505f913a2d93e54ee8c4a82f1ffec273d6
NandhiniManohar22 <51949323+NandhiniManohar22@users.noreply.github.com>
2019-07-20 14:52:53 +0530

    Update 6-2.py

--- 6-2.py
+++ 6-2.py
@@ -1,4 +1,4 @@
-char=["one","two","three","four","five","six","seven","eight","nine","ten"]
+char=["One","Two","Three","Four","Five","six","Seven","Eight","Nine","Ten"]
 nan=int(input())
-if (nan>1 and nan<=10):
+if (nan>=1 and nan<=10):
    print(char[nan-1])
